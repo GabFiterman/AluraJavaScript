e65489da1cfda173726db62cf64055eb1c5ef99a
Fiterman <gfiterman96@gmail.com>
2022-01-23 20:47:13 -0300

    Aula 1

--- ByteBank/ContaCorrente.js
+++ ByteBank/ContaCorrente.js
@@ -2,76 +2,48 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
     static numeroDeContas = 0;
-    agencia;
-    _cliente;
-    _saldo = 0; // _saldo =0 https://github.com/tc39/proposal-class-fields_private-fields
-
-    //#region Getters and Setters    
-    set cliente(novoCliente){
-        if(novoCliente instanceof Cliente){
-            this._cliente = novoCliente;
-        } else{
-            console.error("Valor inválido para o cliente!");
+    
+    
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
         }
     }
 
-    get cliente(){
+    get cliente() {
         return this._cliente;
     }
 
-    
-    get saldo(){
+    get saldo() {
         return this._saldo;
     }
 
-    constructor(agencia, cliente){
+    constructor(agencia, cliente) {
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
     }
-    //#endregion
 
-    //#region Métodos
-    /** Saca um valor da conta
-     * @param {valor} valor 
-     * @returns 
-     * @author Gabriel
-     */
-    sacar(valor){
-        console.log("+++++ Sacando o valor de R$:" + valor + " +++++");
-        if(this._saldo >= valor){
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
             this._saldo -= valor;
-            console.log("Operação concluída com sucesso!\nSeu saldo é R$:" + this._saldo);
             return valor;
-        } else {
-            console.log("Saldo insuficiente!\nSeu saldo é R$:" + this._saldo);
         }
     }
 
-    /** Deposita um valor na conta
-     * @param {valor} valor 
-     * @returns 
-     * @author Gabriel
-     */
-    depositar(valor){
-        console.log("+++++ Depositando o valor de de R$:" + valor + "+++++");
-        if(valor <= 0){
-            console.log("ERRO! só consigo depositar valores positivos!");
+    depositar(valor) {
+        if (valor <= 100) {
             return;
-        } else {
-            this._saldo += valor;
-            console.log("Operação concluída com sucesso!\nSeu saldo é R$: " + this._saldo);
         }
+        this._saldo += valor;
     }
 
-    /** Transfere da conta 'A' para a conta 'B'
-     * @param {valor} valor 
-     * @param {conta} conta
-     * @author Gabriel
-     */
-    transferir(valor, conta){
+    tranferir(valor, conta) {
+
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
+
     }
-    //#endregion
 }
--- ByteBank/index.js
+++ ByteBank/index.js
@@ -1,16 +1,17 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
+import {ContaPoupanca} from "./ContaPoupanca.js";
+import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Gabriel", "04236059177");
-const cliente2 = new Cliente("Kalora", "88.822.233-09");
 
-const conta1 = new ContaCorrente(1001, cliente1);
+const conta1 = new Conta(0, cliente1, 1001);
+
 conta1.depositar(500);
-const conta2 = new ContaCorrente(1002, cliente2);
+conta1.sacar(100);
 
-let valor = 200
-conta1.transferir(valor, conta2);
+const ContaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
-console.log("--------------------");
-console.log("Conta 1: \n", conta1, "\nConta2: \n", conta2);
-console.log("Existem: " + ContaCorrente.numeroDeContas + " contas ativas");
+console.log("--------------------"); 
+console.log(ContaPoupanca);
+console.log(conta1);
