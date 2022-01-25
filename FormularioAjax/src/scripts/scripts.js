a6512fe9c6ff19954c2ebbe3c8eb55bddbbbb582
Fiterman <gfiterman96@gmail.com>
2022-01-24 23:23:03 -0300

    Ajax Form
    Consumo de API de busca de CEP (API ViaCep), com um formulário simples utilizando o framework stilize.

---
+++ FormularioAjax/src/scripts/scripts.js
@@ -0,0 +1,27 @@
+function buscaCep() {
+    let inputCep = document.querySelector('input\[name=cep\]');
+    let cep = inputCep.value.replace('-', '');
+    let url = 'http://viacep.com.br/ws/' + cep + '/json';
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function () {
+        // preencheCampos(JSON.parse(xhr.responseText));
+        if (xhr.readyState == 4) {
+            if (xhr.status = 200)
+                preencheCampos(JSON.parse(xhr.responseText));
+        }
+    }
+    xhr.send();
+
+    console.log(`ReadyState: ${xhr.readyState}\nStatus: ${xhr.status}`);
+    console.log(`Cep: ${cep}\nUrl: ${url}\nXHR: ${xhr}`);
+
+}
+
+function preencheCampos(json) {
+    document.querySelector('input[name=endereco]').value = json.logradouro;
+    document.querySelector('input[name=bairro]').value = json.bairro;
+    document.querySelector('input[name=complemento]').value = json.complemento;
+    document.querySelector('input[name=cidade]').value = json.localidade;
+    document.querySelector('input[name=estado]').value = json.uf;
+}
